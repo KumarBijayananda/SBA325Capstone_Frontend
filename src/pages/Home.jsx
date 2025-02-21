21d5cb4384749c971ddf78fa23a31c7ffc2936f2
Kumar Bijayananda <kumarbijayananda@gmail.com>
2025-02-22 04:01:43 +0545

    Home Page connected and other blank pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+export default function Home(){
+    return(
+        <>
+        <h1>Welcome to Draftrove</h1>
+        <h3>Login or Sign up if you don't already have an account.</h3>
+        <p>You can create documents for yours ideas and save it with version control where you can always
+            go back to your older version. Each save will remain until you delete it manually.
+        </p>
+        </>
+    )
+}
