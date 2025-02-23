b19b5580df3135be844dc4cd1d4bc97fb7632854
Kumar Bijayananda <kumarbijayananda@gmail.com>
2025-02-23 09:21:55 +0545

    dark mode css update and add protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 //Dependencies
-import { Routes, Route, Link } from 'react-router-dom';
-import './App.css'
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
-import Draft from './pages/Draft';
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Draft from "./pages/Draft";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={< Login/>}/>
-      <Route path ='/signup' element={< Signup/>}/>
-      <Route path ='/dashboard' element={<Dashboard/>}/> 
-      <Route path ='/draft' element={< Draft/>}/>
-      <Route path='*' element={<h1>404 Not Found</h1>} />
-
-    </Routes>
-     
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/draft" element={<Draft />} />
+        </Route>
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ProtectedRoutes.jsx
@@ -0,0 +1,9 @@
+import {Outlet} from "react-router-dom";
+import {useAuth} from "../context/auth/auth_context";
+
+export default function ProtectedRoutes(){
+    console.log("protected")
+
+    const {cookies} = useAuth();
+    return cookies.token?<Outlet/>: <h1>You are not Authorized to view</h1>
+}
