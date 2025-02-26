4feb5842c1c31150926fb2fb32631909e4c3cd96
Kumar Bijayananda <kumarbijayananda@gmail.com>
2025-02-26 13:37:55 -0500

    adding btn for inspiring image for writing prompt from third party API call

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -1,5 +1,48 @@
-export default function ImageModal(){
+import { useEffect, useState } from "react";
+import axios from   "axios";
+
+export default function ImageModal({ isOpen, onClose }){
+    const[photos, setPhotos]=useState([]);
+    const API_KEY="3NhsMzZjROb3VBdXXMjm5Vt9XDLMVmbDNQMZcPSAS2fAL6YW1quNdBwx";
+
+    useEffect(()=>{
+        if(isOpen){
+            getImages(randomPage, 6);
+        }
+    },[isOpen])
+
+    async function getImages(page=1, perPage=6){
+        try {
+            const res= await axios("https://api.pexels.com/v1/curated", {
+        params: { page, per_page: perPage },
+        headers: { Authorization: API_KEY },
+      });
+      setPhotos(res.data.photos);
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+    const randomPage = Math.floor(Math.random() * 50) + 1;
+    if(!isOpen) return null;
+    
     return (
-        <></>
+        <>
+        <div className="modalContainer">
+            <div className="modalBox">
+                <button className="closeBtn" onClick={onClose}>X</button>
+                <h3>Images for your inspiration</h3>
+                <div className="images">
+                    {
+                        photos.map((photo)=>(
+                            <img key={photo.id} src={photo.src.medium} alt="inspiring image" />
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+        
+        </>
     )
+    
 }
--- src/components/LogoutBtn.jsx
+++ src/components/LogoutBtn.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/auth/auth_context";
+import ImageModal from "./ImageModal";
 
 export default function LogoutBtn() {
   const nav = useNavigate();
   const { logout } = useAuth();
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   function handleDash() {
     nav("/dashboard");
@@ -15,7 +17,7 @@ export default function LogoutBtn() {
     nav("/");
   }
   function handleInsp(){
-    
+setIsModalOpen(true);
   }
   return (
     <>
@@ -23,6 +25,7 @@ export default function LogoutBtn() {
         Dashboard
       </button>
       <button onClick={handleInsp}>Click here to get inspiration</button>
+      <ImageModal isOpen={isModalOpen} onClose={()=>setIsModalOpen(false)} />
       <button className="logoutBtn" onClick={handleLogout}>
         Logout
       </button>
